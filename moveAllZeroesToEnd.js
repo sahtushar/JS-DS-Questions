ba6f1b963e7b8514fc3eb9d71399e3e98d3edd63
Tushar Sah <sahtushar@gmail.com>
2019-06-28 20:53:37 +0530

    first commit from pc

--- moveAllZeroesToEnd.js
+++ moveAllZeroesToEnd.js
@@ -1,13 +1,14 @@
 //let arr=[1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9, 0];
-let arr=[2,5,0,0,1,0,0];
+let arr=[2,5,0,0,1,0,0,7,1,2,5,0,0,0,2];
 let k;
-for (let i=0;i<arr.length;i++) {
+for (let i=arr.length-1;i>=0;i--) {
     if(arr[i]!==0)
     {
         k=i;
         break;
     }
 }
+console.log(k);
 let z=k;
 for(let i=0;i<=k;i++)
 {
@@ -20,6 +21,8 @@ for(let i=0;i<=k;i++)
     }
 }
 
-for (let i=arr.length-1;i>=0;i--) {
-   console.log(arr[i]);
+let result="";
+for (let i=0;i<arr.length;i++) {
+  result+=arr[i]+' ';
 }
+console.log(result);
