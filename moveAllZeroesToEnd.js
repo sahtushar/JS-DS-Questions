15b3845636e6c627a19be260a44afceca864c424
Tushar Sah <sahtushar@gmail.com>
2019-06-28 22:03:21 +0530

    move all zeroes to end -done

--- moveAllZeroesToEnd.js
+++ moveAllZeroesToEnd.js
@@ -1,28 +1,52 @@
 //let arr=[1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9, 0];
-let arr=[2,5,0,0,1,0,0,7,1,2,5,0,0,0,2];
+let arr=[0,0,1,0,0,5,0,0,1,2,3,0];
 let k;
-for (let i=arr.length-1;i>=0;i--) {
-    if(arr[i]!==0)
+for (let i=0;i<arr.length;i++) {
+    if(arr[i]===0 )
     {
         k=i;
         break;
     }
 }
-console.log(k);
-let z=k;
-for(let i=0;i<=k;i++)
-{
-    if(arr[i]===0)
+let l;
+for (let i=0;i<arr.length;i++) {
+    if(arr[i]!==0 && arr[i-1]===0)
     {
-        let x=arr[z];
-        arr[z]=arr[i];
-        arr[i]=x;
-        ++z;
+        l=i;
+        break;
+    }
+}
+
+console.log(l);
+
+
+for(let i=k;i<arr.length;i++)
+{
+    if(l==arr.length)
+    {
+        break;
+    }
+    if(arr[l]!==0){
+
+        let x=arr[k];
+        arr[k]=arr[l];
+        arr[l]=x;
+        ++k;
+        ++l;
+    }
+    else{
+        ++l;
     }
 }
 
 let result="";
-for (let i=0;i<arr.length;i++) {
-  result+=arr[i]+' ';
+for(let i=0;i<arr.length;i++)
+{
+    result+=arr[i]+" ";
 }
 console.log(result);
+
+
+
+
+
